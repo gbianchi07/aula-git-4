a5d048e9185ffa7650605e3aeb67641b302187f3
gbianchi07 <velonisqui1234@gmail.com>
2023-04-03 10:45:21 -0300

    utilizando switch

---
+++ cp.js
@@ -0,0 +1,18 @@
+let opcao = parseInt(prompt("digite uma opcao"));
+
+switch(opcao){
+    case 1:
+        document.write("primeira opcao")
+        break;
+    case 2:
+        document.write("segunda opcao")
+        break;
+    case 3:
+        document.write("terceira opcao")
+        break;
+    default:
+        document.write("nenhuma das opcoes validas")
+        break;
+    
+
+}
