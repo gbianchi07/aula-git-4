d657057923efc214f432d6c1c20f46316dc1f301
gbianchi07 <velonisqui1234@gmail.com>
2023-04-03 11:21:33 -0300

    utilizando o for

--- cp.js
+++ cp.js
@@ -1,3 +1,4 @@
+/*switch
 let opcao = parseInt(prompt("digite uma opcao"));
 
 switch(opcao){
@@ -13,6 +14,33 @@ switch(opcao){
     default:
         document.write("nenhuma das opcoes validas")
         break;
-    
+    }
+*/
+// incremnto e decremento
+let numero = 7
+document.write(numero)
+valor = numero+1
+document.write("<br><br>")
+numero= numero+1
+document.write(numero)
+//decretando valores
+let numero1 = 7
+document.write(numero1)
+valor = numero-1
+document.write("<br><br>")
+numero1= numero-1
+document.write(numero1)
 
+// for
+
+for (let i=0; i<10; i++){
+    document.write("o valor i vai ate:"+i);
+    document.write("<br><br>")
+     }
+
+let carros=["gol","fusca","hb20","corsa"]
+
+for(let i=0; i < carros.length;i++){
+    document.write(carros[i]);
+    document.write("<br><br>")
 }
